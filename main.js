7ef143dcabddfe6e036c4b0af90946ac87b28325
notmoff <applering333@gmail.com>
2026-02-16 21:08:19 +0900

    フォームの送信内容を回文テストするように変更

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 let Phrase = require("moff-palindrome");
 
-let string = prompt("パリンドロームをテストしたい文字列を入力してください:");
-let phrase = new Phrase(string);
+function palindromeTester(event) {
+  event.preventDefault();
 
-if (phrase.palindrome()) {
-  alert(`${phrase.content}は、パリンドロームです`);
-} else {
-  alert(`${phrase.content}は、パリンドロームではありません`);
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"${phrase.content}"はパリンドロームです！`;
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}"はパリンドロームではありません`;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event) {
+    palindromeTester(event);
+  });
+});
